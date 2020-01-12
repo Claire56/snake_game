69cb44d040d29dafcd4c4cd94de0966b84775408
claire muwanguzi <kclaire56@yahoo.com>
2020-01-12 14:27:47 -0800

    added curses.py

---
+++ curses.py
@@ -0,0 +1,8 @@
+import curses
+from curses import KEY_LEFT, KEY_RIGHT,KEY_UP,KEY_DOWN
+# from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+import random as r
+
+
+curses.initscr()
+window= curses.newwin(10,50,0,0)
