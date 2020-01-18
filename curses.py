8c7ed2f6700bf290dca02569d0bc55d78be05f45
claire muwanguzi <kclaire56@yahoo.com>
2020-01-17 22:12:57 -0800

    added scores, food, pen, and movements

--- curses.py
+++ curses.py
@@ -5,10 +5,13 @@ import random
 wn = turtle.Screen()
 wn.bgcolor('purple')
 wn.title('Claire_codes Snake game')
-wn.setup(height=500, width=500)
+wn.setup(height=600, width=600)
 wn.tracer(0) #turns off animation on the screen and updates
+
 delay =0.3
-score=o
+score= 0
+high_score =0
+tails=[]
 
 #create snake
 snake = turtle.Turtle()
@@ -26,6 +29,16 @@ food.shape('circle')
 food.penup()
 food.speed(0)
 food.goto(random.randint(10,100), random.randint(10,100))
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
 
 
 #Functions 
@@ -67,6 +80,34 @@ wn.onkeypress(go_left,'a')
 
 while True:
     wn.update()
+    if snake.distance(food)<=20:
+        food.goto(random.randint(-290,290), random.randint(-290,290))
+
+        # Add a segment to the snake
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        tails.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+    # Move the end tails first in reverse order
+    for index in range(len(tails)-1, 0, -1):
+        x = tails[index-1].xcor()
+        y = tails[index-1].ycor()
+        tails[index].goto(x, y) #current tail is moved to the prev tail location
+
+    # Move segment 0 to where the snahe head  is
+    if len(tails) > 0:
+        x = snake.xcor()
+        y = snake.ycor()
+        tails[0].goto(x,y)
     move()
     time.sleep(delay)
 
